e72cf24672260cec030b694244a3da0191194538
Daian <daian.aiziatov@gmail.com>
2018-10-09 11:52:14 -0400

    Day9

--- lighthouse.js
+++ lighthouse.js
@@ -122,3 +122,17 @@ function isCurrent(coordinate) {
 }
 
 console.log(isCurrent('E2'));
+
+/*Challenge 9)And last, but certainly not least, there are cells with ships in them! We need to be able to query any cell in our GRID area and find out if it is occupied by a ship.
+
+Write a function called isShip() which will take in a coordinate in the form of 'I9' and return a true or a false boolean value depending on whether there is a ship in that cell or not. (Example: isShip('B3') would return true)*/
+
+function isShip(coordinate) {
+	if (lightCell(coordinate) == 'v') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isShip('B3'));
